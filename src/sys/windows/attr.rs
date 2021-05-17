bdfbeec41f3a6eeffb2bf49cdf3c8d4d24740527
Steven Stanfield <stanfield@scarecrowtech.com>
2021-05-17 17:45:17 +0000

    Try to fix escape code processing on windows.

--- src/sys/windows/attr.rs
+++ src/sys/windows/attr.rs
@@ -6,15 +6,22 @@ use super::Termios;
 pub fn get_terminal_attr() -> io::Result<Termios> {
     let console_mode = ConsoleMode::from(Handle::current_in_handle()?);
 
-    let mode = console_mode.mode()?;
+    let in_mode = console_mode.mode()?;
 
-    Ok(Termios(mode))
+    let console_mode = ConsoleMode::new()?;
+
+    let out_mode = console_mode.mode()?;
+    Ok(Termios(in_mode, out_mode))
 }
 
 pub fn set_terminal_attr(termios: &Termios) -> io::Result<()> {
     let console_mode = ConsoleMode::from(Handle::current_in_handle()?);
 
     console_mode.set_mode(termios.0)?;
+
+    let console_mode = ConsoleMode::new()?;
+
+    console_mode.set_mode(termios.1)?;
 
     Ok(())
 }
@@ -32,4 +39,7 @@ pub fn raw_terminal_attr(termios: &mut Termios) {
     const RAW_MODE_MASK: u32 = ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT | ENABLE_PROCESSED_INPUT;
 
     termios.0 &= !RAW_MODE_MASK;
+
+    const ENABLE_VIRTUAL_TERMINAL_PROCESSING: u32 = 0x0004;
+    termios.1 |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
 }
--- src/sys/windows/mod.rs
+++ src/sys/windows/mod.rs
@@ -1,7 +1,7 @@
 extern crate crossterm_winapi;
 
 #[derive(Clone, Copy, Debug)]
-pub struct Termios(u32);
+pub struct Termios(u32, u32); // (input flags, output flags)
 
 pub mod attr;
 pub mod size;
