1665f77c561ac085001d3272deb3978b18fbebc2
Steven Stanfield <stanfield@scarecrowtech.com>
2021-05-28 04:14:58 +0000

    Patch up windows console.

--- src/sys/windows/console.rs
+++ src/sys/windows/console.rs
@@ -1,24 +1,19 @@
 //! Support async reading of the tty/console.
 
-use std::io::{self, Read, Stdin, Stdout, Write};
-use std::sync::{Mutex, MutexGuard};
+use std::fs::{File, OpenOptions};
+use std::io::{self, Read, Write};
 use std::thread;
 use std::time::Duration;
 
 use crossbeam_channel::*;
-use lazy_static::lazy_static;
 
 use super::Termios;
 use crate::sys::attr::{get_terminal_attr, raw_terminal_attr, set_terminal_attr};
-use crate::sys::tty::{get_tty, set_virtual_terminal};
+use crate::sys::tty::set_virtual_terminal;
 
-type Internals = io::Result<Mutex<Receiver<io::Result<u8>>>>;
-
-fn setup_tty() -> Internals {
-    //let stdin = io::stdin();
+/// Open and return the read side of a console.
+pub fn open_syscon_in() -> io::Result<SysConsoleIn> {
     set_virtual_terminal()?;
-    //let stdout = io::stdout();
-    //if let Ok(tty) = get_tty() {
     let (send, recv) = unbounded();
     thread::spawn(move || {
         for i in io::stdin().bytes() {
@@ -27,56 +22,42 @@ fn setup_tty() -> Internals {
             }
         }
     });
-    Ok(Mutex::new(recv))
-    //} else {
-    //    panic!("No tty!");
-    //}
+    Ok(SysConsoleIn {
+        recv,
+    })
 }
 
-lazy_static! {
-    // Provide a protected singleton for the tty.  There is only one so try to
-    // enforce that to avoid a myriad of issues.
-    //static ref INTERNAL_TTY: Mutex<Internals> = Mutex::new(setup_tty());
-    static ref INTERNAL_TTY: Internals = setup_tty();
+/// Open and return the write side of a console.
+pub fn open_syscon_out() -> io::Result<SysConsoleOut> {
+    let tty = OpenOptions::new().write(true).open("CONOUT$")?;
+    Ok(SysConsoleOut {
+        tty,
+        prev_ios: None,
+    })
 }
 
-/// Construct an asynchronous handle to the TTY standard input.
-///
-/// This allows you to read from standard input _without blocking_ the current thread.
-/// Specifically, it works by firing up another thread to handle the event stream, which will then
-/// be buffered in a mpsc queue, which will eventually be read by the current thread.
-///
-/// This will not read the piped standard input, but rather read from the TTY device, since reading
-/// asyncronized from piped input would rarely make sense. In other words, if you pipe standard
-/// output from another process, it won't be reflected in the stream returned by this function, as
-/// this represents the TTY device, and not the piped standard input.
-pub fn sys_console<'a>() -> io::Result<SysConsole<'a>> {
-    match &*INTERNAL_TTY {
-        Ok(recv) => Ok(SysConsole {
-            recv: recv.lock().unwrap(),
-            prev_ios: None,
-        }),
-        Err(err) => Err(io::Error::new(err.kind(), err)),
-    }
+/// Represents system specific part of a tty/console output.
+pub struct SysConsoleOut {
+    tty: File,
+    prev_ios: Option<Termios>,
 }
 
 /// An asynchronous reader.
 ///
 /// This acts as any other stream, with the exception that reading from it won't block. Instead,
 /// the buffer will only be partially updated based on how much the internal buffer holds.
-pub struct SysConsole<'a> {
+pub struct SysConsoleIn {
     /// The underlying receiver.
-    recv: MutexGuard<'a, Receiver<io::Result<u8>>>,
-    prev_ios: Option<Termios>,
+    recv: Receiver<io::Result<u8>>,
 }
 
-impl<'a> Drop for SysConsole<'a> {
+impl Drop for SysConsoleOut {
     fn drop(&mut self) {
         if self.suspend_raw_mode().is_err() {}
     }
 }
 
-impl<'a> SysConsole<'a> {
+impl SysConsoleOut {
     /// Temporarily switch to original mode
     pub fn suspend_raw_mode(&self) -> io::Result<()> {
         if let Some(prev_ios) = self.prev_ios {
@@ -95,7 +76,9 @@ impl<'a> SysConsole<'a> {
         }
         Ok(())
     }
+}
 
+impl SysConsoleIn {
     /// Return when more data is avialable.
     ///
     /// Calls to a get_* function should return a value now.
@@ -117,7 +100,7 @@ impl<'a> SysConsole<'a> {
     }
 }
 
-impl<'a> Read for SysConsole<'a> {
+impl Read for SysConsoleIn {
     /// Read from the byte stream.
     ///
     /// This will never block, but try to drain the event queue until empty. If the total number of
@@ -148,14 +131,14 @@ impl<'a> Read for SysConsole<'a> {
     }
 }
 
-impl<'a> Write for SysConsole<'a> {
+impl Write for SysConsoleOut {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        //self.tty.write(buf)
-        io::stdout().write(buf)
+        self.tty.write(buf)
+        //io::stdout().write(buf)
     }
 
     fn flush(&mut self) -> io::Result<()> {
-        //self.tty.flush()
-        io::stdout().flush()
+        self.tty.flush()
+        //io::stdout().flush()
     }
 }
